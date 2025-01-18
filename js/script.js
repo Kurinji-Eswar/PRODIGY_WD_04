1e59126c63d82a171ddc7c278c89b6eb68b2d0ed
Kurinji-Eswar <kurinjieswar05@gmail.com>
2025-01-18 17:27:25 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function updateDate() {
+    const now = new Date();
+    const options = { 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric' 
+    };
+    const dateString = now.toLocaleDateString('en-US', options);
+    document.getElementById('date').textContent = dateString;
+}
+updateDate();
